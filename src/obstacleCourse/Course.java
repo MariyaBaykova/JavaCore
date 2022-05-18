6f591e5bed386a8ef30bae6a90e6d38d1ffeebe4
ABaykov <alexandr.baykov@gmail.com>
2022-05-18 17:20:03 +0300

    Initial commit.

---
+++ src/obstacleCourse/Course.java
@@ -0,0 +1,24 @@
+package obstacleCourse;
+
+import Main.Status;
+import team.Team;
+import team.TeamMember;
+
+public class Course {
+    private Obstacle[] obstacles;
+
+    public Course(Obstacle... obstacles) {
+        this.obstacles = obstacles;
+    }
+
+    public void doIt(Team team) {
+        for (TeamMember member: team.getMembers()) {
+            for (Obstacle obs : obstacles) {
+                obs.goChallenge(member);
+                if (member.getStatus() == Status.NoPassedDistance) {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ src/obstacleCourse/Cross.java
@@ -0,0 +1,14 @@
+package obstacleCourse;
+
+import team.TeamMember;
+
+public class Cross extends Obstacle {
+    public Cross(int difficulty) {
+        super(difficulty);
+    }
+
+    @Override
+    public void goChallenge(TeamMember member) {
+        member.run(super.getDifficulty());
+    }
+}
