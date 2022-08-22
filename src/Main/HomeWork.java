cfba78140687353b95acff81542f8949282afe40
ABaykov <alexandr.baykov@gmail.com>
2022-08-22 19:49:57 +0300

    lesson-4

---
+++ src/Main/HomeWork.java
@@ -0,0 +1,15 @@
+package Main;
+
+import java.util.Arrays;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        AppData data = new AppData();
+        data.readFromFile("example.csv");
+        System.out.println(Arrays.toString(data.getHeader()));
+        System.out.println(Arrays.deepToString(data.getData()));
+
+        data.writeToCSV("new_example.csv");
+    }
+}
